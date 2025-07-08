55029f7bbde0fd8d77ccd24ff96a86317d60db25
SonnyGitau <sonnygitau@gmail.com>
2025-07-08 13:16:44 +0300

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,15 +5,15 @@ time_bound = input("Is the task time-bound? (yes or no): ").lower()
 # Provide a customized reminder using match case
 match priority:
     case "high":
-        reminder = f"Reminder: You have a HIGH priority task - '{task}'."
+        reminder = f"Reminder: You have a high priority task - '{task}'."
     case "medium":
-        reminder = f"Reminder: You have a MEDIUM priority task - '{task}'."
+        reminder = f"Reminder: You have a medium  priority task - '{task}'."
     case "low":
-        reminder = f"Reminder: You have a LOW priority task - '{task}'."
+        reminder = f"Reminder: You have a low priority task - '{task}'."
     case _:
          reminder = f"Reminder: You have a task - '{task}' with unspecified priority."
      # Modify message if the task is time-bound
 if time_bound == "yes":  
-    reminder += " This is time-sensitive that requires immediate attention today!"
+    reminder += " This is time-sensitive task that requires immediate attention today!"
 # Print the customized reminder
 print(reminder)
