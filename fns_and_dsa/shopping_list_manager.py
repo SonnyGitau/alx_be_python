29ea0c0e711b0c265f5065434056fa96d9cd7fea
SonnyGitau <sonnygitau@gmail.com>
2025-07-10 21:36:30 +0300

    Explore `datetime` Module

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
 def display_menu():
     """Display the shopping list menu."""
-    print(f"Shopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
