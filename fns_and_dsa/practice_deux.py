864692d2594494e839cd1d23ca273d88b9e6b56b
SonnyGitau <sonnygitau@gmail.com>
2025-07-10 20:13:59 +0300

    practice runs

---
+++ fns_and_dsa/practice_deux.py
@@ -0,0 +1,7 @@
+#calculate the area of a rectangle
+def calculate_area(length, width):
+    area = length * width
+    return area
+# Call the function with sample values
+result = calculate_area(57, 30)
+print(f"The area of the rectangle is {result} square units.")
---
+++ fns_and_dsa/practice_runs.py
@@ -0,0 +1,6 @@
+#define function 
+def greet_user(name):
+    message = f" Hello, {name}! Welcome to the program."
+    print(message)
+# Call the function with a sample name
+greet_user("Sonny")
